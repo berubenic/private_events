e76e15e957817fec976203e650b1c0901e577134
Nicholas <berubenic@gmail.com>
2021-02-01 11:04:27 -0500

    Created Sessions(New, Create, Delete). Simple Login with user.name.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,15 @@
+class SessionsController < ApplicationController
+  def new; end
+
+  def create
+    user = User.find_by(name: params[:name])
+    if user
+      session[:user_id] = user.id
+      redirect_to user
+    else
+      redirect_to login_url
+    end
+  end
+
+  def destroy; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  # resources :sessions, only: %i[new create delete]
+  controller :sessions do
+    get 'login' => :new
+    post 'login' => :create
+    delete 'logout' => :destroy
+  end
+
+  get 'sessions/create'
+  get 'sessions/destroy'
   resources :users, only: %i[new create show]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html

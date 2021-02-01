086e85592987f02338ed4c946ec316de2d956fbc
Nicholas <berubenic@gmail.com>
2021-02-01 10:21:34 -0500

    New User, Create User and Show User

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,18 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(name: params[:user][:name])
+    if @user.save
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+
+  def show
+    @users = User.all 
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get 'user/new'
-  get 'user/create'
-  get 'user/show'
+  resources :users, only: %i[new create show]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

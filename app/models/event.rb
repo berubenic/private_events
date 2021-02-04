cb6f0222d402654ea1c2327ff2293f3673baf69e
Nicholas <berubenic@gmail.com>
2021-02-04 16:18:05 -0500

    User and Event validations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,15 +1,11 @@
 class Event < ApplicationRecord
+  validates :name, :body, :location, :date, presence: true
+  validates :name, :location, length: { maximum: 25 }
+  validates :body, length: { minimum: 10 }
+
   belongs_to :creator, class_name: 'User', foreign_key: :creator_id
   has_and_belongs_to_many :attendees, class_name: 'User'
 
   scope :past, -> { where('date < ?', DateTime.now) }
   scope :upcoming, -> { where('date > ?', DateTime.now) }
-
-  # def past
-  #  date < DateTime.now
-  # end
-
-  # def upcoming
-  #  date > DateTime.now
-  # end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  validates :name, uniqueness: true, presence: true
+
   has_many :events, foreign_key: :creator_id
   has_and_belongs_to_many :attended_events, class_name: 'Event'
 end

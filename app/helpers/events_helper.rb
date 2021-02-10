1bce5c755f47a299820bc11806c06e99388fbdb0
Nicholas <berubenic@gmail.com>
2021-02-10 18:54:58 -0500

    Events#show, displays available invites, pending and declined, #users_available_to_invite helper method

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,16 @@
 module EventsHelper
+  def users_available_to_invite
+    invited_users = @event.pending_attendees
+    declined_users = @event.declined_attendees
+    attendees = @event.attendees
+    users = User.all
+    users.filter_map do |user|
+      unless invited_users.include?(user) ||
+             declined_users.include?(user) ||
+             attendees.include?(user) ||
+             @event.creator == user
+        user
+      end
+    end
+  end
 end
